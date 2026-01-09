ccd0e69978169952b1c976ef7c72f652d1e33c7a
Vladislav <vladudosc@gmail.com>
2026-01-09 15:38:33 +0300

    test45

--- Hello.py
+++ Hello.py
@@ -3,3 +3,9 @@ def get_vowels(String):
 get_vowels("animal") # [a, i, a]
 get_vowels("sky") # []
 get_vowels("football") # [o, o, a]
+
+def check_duplicate(lst):
+    return len(lst) != len(set(lst))
+check_duplicate([1,2,3,4,5,4,6]) # True
+check_duplicate([1,2,3]) # False
+check_duplicate([1,2,3,4,9]) # False
